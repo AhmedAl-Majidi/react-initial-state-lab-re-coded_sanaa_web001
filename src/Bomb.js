470fe599c1e9ec751f18e6399dd40d5aeed5c2dc
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-15 05:36:43 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -5,14 +5,14 @@ export default class Bomb extends Component {
         super()
         this.state = { secondsLeft : props.initialCount }
     }
-  
-       
-    
-    render() {      
+
+
+
+    render() {
         const text = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
         return (
             <div>
-                {text} 
+                {text}
             </div>
         )
     }
